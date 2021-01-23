e1028c7d94e7222a2ac7b256c966c393bb43a0a0
MichaelOgundero <ogunderomichael@gmail.com>
2021-01-22 19:57:19 -0600

    added a footer at the bottom of the page

--- blog-website/src/modules/Footer/index.js
+++ blog-website/src/modules/Footer/index.js
@@ -1,6 +1,51 @@
+import React, {useState} from "react";
+import PropTypes from "prop-types"
+import {Flex, Box} from "rebass"
+import { useMediaQuery } from "react-responsive";
+
+
 const Footer = () => {
-    return <div>this is footer</div>;
+
+  const [paddingFooter, setPaddingFooter] = useState(0)
+
+  const handlePaddingFooter = (matches) => {
+    if (matches) {
+      setPaddingFooter(6);
+    } else {
+      setPaddingFooter(4);
+    }
   };
+
+  const isMobile = useMediaQuery({ maxDeviceWidth: 768 });
+  const isLaptop = useMediaQuery(
+    { minDeviceWidth: 769 },
+    undefined,
+    handlePaddingFooter
+  );
+    return(
+      <div>
+        <Flex
+          px = {paddingFooter}
+          py={5}
+          sx={{
+            position: "relative",
+            width: "100%",
+            background: "black",
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          {"Footer"}
+        </Flex>
+
+      </div>
+
+    );
+  };
+
+Footer.propTypes = {
+
+}
   
-  export default Footer;
+export default Footer;
   
