42b491e7f85d69da21437158c0411d455fa06916
MichaelOgundero <ogunderomichael@gmail.com>
2021-01-26 17:53:41 -0600

    started working on the footer

--- blog-website/src/modules/Footer/index.js
+++ blog-website/src/modules/Footer/index.js
@@ -1,12 +1,13 @@
-import React, {useState} from "react";
-import PropTypes from "prop-types"
-import {Flex, Box} from "rebass"
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Flex, Box } from "rebass";
 import { useMediaQuery } from "react-responsive";
-
+import twitterIcon from "../../assets/icons/twitter-black-24p.svg";
+import facebookIcon from "../../assets/icons/facebook-black-24p.svg";
+import instagramIcon from "../../assets/icons/instagram-black-24p.svg";
 
 const Footer = () => {
-
-  const [paddingFooter, setPaddingFooter] = useState(0)
+  const [paddingFooter, setPaddingFooter] = useState(0);
 
   const handlePaddingFooter = (matches) => {
     if (matches) {
@@ -22,30 +23,192 @@ const Footer = () => {
     undefined,
     handlePaddingFooter
   );
-    return(
-      <div>
-        <Flex
-          px = {paddingFooter}
-          py={5}
-          sx={{
-            position: "relative",
-            width: "100%",
-            background: "black",
-            color: "white",
-            textAlign: "center",
-          }}
-        >
-          {"Footer"}
-        </Flex>
+  return (
+    <div>
+      <Flex
+        flexDirection="column"
+        justifyContent="center"
+        px={paddingFooter}
+        py={5}
+        sx={{
+          position: "relative",
+          background: "black",
+          color: "white",
+          textAlign: "center",
+          marginTop: "80px",
+        }}
+      >
+        {isLaptop && (
+          <div>
+            <Flex
+              my={2}
+              flexDirection="row"
+              justifyContent="center"
+              sx={{
+              }}
+            >
+              <Box
+                color="white"
+                fontSize = {4}
+              >
+                {"Title"}
+              </Box>
+            </Flex>
 
-      </div>
+            <Flex
+              flexDirection="row"
+              justifyContent="center"
+              pt={4}
+              sx={{
+              }}
+            >
+              <Box
+                width="50px"
+                height="50px"
+                m={2}
+                sx={{
+                  backgroundColor: "white",
+                  borderRadius: "50%",
+                  display: "flex",
+                  justifyContent: "center",
+                  ":hover": {
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                <img alt="twitter icon" src={twitterIcon} />
+              </Box>
 
-    );
-  };
+              <Box
+                width="50px"
+                height="50px"
+                m={2}
+                sx={{
+                  backgroundColor: "white",
+                  borderRadius: "50%",
+                  display: "flex",
+                  justifyContent: "center",
+                  ":hover": {
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                <img alt="twitter icon" src={facebookIcon} />
+              </Box>
 
-Footer.propTypes = {
+              <Box
+                width="50px"
+                height="50px"
+                m={2}
+                sx={{
+                  backgroundColor: "white",
+                  borderRadius: "50%",
+                  display: "flex",
+                  padding: "12px",
+                  justifyContent: "center",
+                  ":hover": {
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                <img alt="twitter icon" src={instagramIcon} />
+              </Box>
+            </Flex>
 
-}
-  
+            <Flex
+              mt={3}
+              flexDirection="row"
+              justifyContent="center"
+              sx={{
+              }}
+            >
+              <Box
+                color="white"
+                fontSize = {2}
+                px={0}
+                sx={{
+                  borderBottom: "1px solid transparent",
+                  ":hover": {
+                    cursor: "pointer",
+                    borderBottom: "1px solid white",
+                    fontWeight: "bold",
+                  }
+                }}
+              >
+                {"Contact"}
+              </Box>
+
+              <Box
+                color="white"
+                fontSize = {2}
+                px={3}
+              >
+                <span>&#8226;</span>
+              </Box>
+
+              <Box
+                color="white"
+                fontSize = {2}
+                px={0}
+                sx={{
+                  borderBottom: "1px solid transparent",
+                  ":hover": {
+                    cursor: "pointer",
+                    borderBottom: "1px solid white",
+                    fontWeight: "bold",
+                  }
+                }}
+              >
+                {"Info"}
+              </Box>
+
+              <Box
+                color="white"
+                fontSize = {2}
+                px={3}
+              >
+                <span>&#8226;</span>
+              </Box>
+
+              <Box
+                color="white"
+                fontSize = {2}
+                px={0}
+                sx={{
+                  borderBottom: "1px solid transparent",
+                  ":hover": {
+                    cursor: "pointer",
+                    borderBottom: "1px solid white",
+                    fontWeight: "bold",
+                  }
+                }}
+              >
+                {"Support"}
+              </Box>
+            </Flex>
+
+            <Flex
+              mt={4}
+              flexDirection="row"
+              justifyContent="center"
+              sx={{
+              }}
+            >
+              <Box
+                color="gray"
+                fontSize = {1}
+              >
+                Copyright &#169; 2020 Title, Inc.
+              </Box>
+            </Flex>
+          </div>
+        )}
+        {isMobile && <div>{"mobile"}</div>}
+      </Flex>
+    </div>
+  );
+};
+
+Footer.propTypes = {};
+
 export default Footer;
-  
