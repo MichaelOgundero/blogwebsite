56031936a8c4f1d8530af1424f227769473dcae5
MichaelOgundero <ogunderomichael@gmail.com>
2021-07-09 21:47:55 -0600

    worked on the grid and added feedback when the cursor interacts with a grid

--- blog-website/src/components/Grid/GridContent.js
+++ blog-website/src/components/Grid/GridContent.js
@@ -48,10 +48,10 @@ const GridContent = ({ display }) => {
                                         borderRadius: "5px",
                                         ":hover": {
                                             cursor: "pointer",
-                                            color: "rgba(93, 63, 211,1)"
+                                            //color: "rgba(93, 63, 211,1)"
                                         },
                                         width: "100%",
-                                        height: "600px",
+                                        height: "100%",
                                         position: "relative",
                                         zIndex: 1
                                     }}>
@@ -62,8 +62,9 @@ const GridContent = ({ display }) => {
                                                 position: "absolute",
                                                 top: "20px",
                                                 right: "20px",
-                                                width: "25px",
-                                                height: "25px",
+                                                width: "20px",
+                                                height: "20px",
+                                                zIndex: 2
                                             }} />
                                         ) : (
                                             <></>
@@ -71,7 +72,7 @@ const GridContent = ({ display }) => {
                                     }
                                     {
                                         data.tracker ? (
-                                            <Image opacity="0.5" alt={"Image " + JSON.stringify(parseInt(index) + 1)} src={data.image} height="250px" display="block" marginLeft="auto" marginRight="auto" width="100%" />
+                                            <Image opacity="0.9" alt={"Image " + JSON.stringify(parseInt(index) + 1)} src={data.image} height="250px" display="block" marginLeft="auto" marginRight="auto" width="100%" />
                                         ) : (
                                             <Image alt={"Image " + JSON.stringify(parseInt(index) + 1)} src={data.image} height="250px" display="block" marginLeft="auto" marginRight="auto" width="100%" />
                                         )
@@ -91,7 +92,9 @@ const GridContent = ({ display }) => {
                                             fontFamily="Merriweather"
                                             fontSize="28px"
                                             sx={{
-                                                paddingBottom: "12px"
+                                                paddingBottom: "12px",
+                                                textDecoration: data.tracker ? "underline":"none"
+                                                
                                             }}
                                         >
                                             {data.title}
@@ -99,11 +102,12 @@ const GridContent = ({ display }) => {
                                         <Text
                                             fontFamily="Raleway"
                                             fontSize="18px"
-                                            paddingBottom="6px"
+                                            paddingBottom="25%"
                                             fontWeight="300"
                                             color="black"
+                                            
                                         >
-                                            {data.introSentence}
+                                            {data.introSentence.slice(0, 65)+"..."}
                                         </Text>
                                     </Box>
                                     <Box
